ba47b5445755f57523a94b301f1e67666c68751c
taehoonhwang <on91716@gmail.com>
2022-01-30 22:45:37 +0900

    v2

--- hello.go
+++ hello.go
@@ -3,6 +3,6 @@ package hello
 import "fmt"
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v, Welcome!", name)
+	message := fmt.Sprintf("[v2] Hi, %v, Welcome!", name)
 	return message
 }
